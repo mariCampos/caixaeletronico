372212bab2ed0aea8af72c52b4f8a52b2774f6fd
mariCampos <marianaalbuquerque02@gmail.com>
2018-03-06 19:02:29 -0300

    Mudando os métodos de sacar e formataTexto

--- saque.js
+++ saque.js
@@ -1,37 +1,58 @@
-const tiposCedulas = [100, 50, 20, 10];
 module.exports = {
     
     sacar(valor) {
          
-        var quantidadeCedulas = [0,0,0,0];
+        //var quantidadeCedulas = [0,0,0,0];
+        var cedulas = [
+            {cedula: 100, quantidade: 0},
+            {cedula: 50, quantidade: 0},
+            {cedula: 20, quantidade: 0},
+            {cedula: 10, quantidade: 0}
+        ]
 
         var total = valor;
         if (valor%10 === 0 && valor >= 10) {
             for(var i = 0; i < 4; i++){
-                while (total >= tiposCedulas[i]) {
-                    total -= tiposCedulas[i];
-                    quantidadeCedulas[i] += 1;
+                while (total >= cedulas[i].cedula) {
+                    total -= cedulas[i].cedula;
+                    cedulas[i].quantidade += 1;
                 } 
             }
         } else {
             return [];
         }
+
+
         
-        return quantidadeCedulas;
+        return cedulas;
     },
 
-    formataTexto(quantidadeCedulas) {
-        var retorno = 'Entregar';
-        if (quantidadeCedulas.length > 0) {
+    formataTexto(cedulas) {
+        var retorno = [];
+        var frase = 'Entregar';
+        if (cedulas.length > 0) {
             for(var j = 0; j < 4; j++) {
-                if (quantidadeCedulas[j] !==0) {
-                    retorno += ` ${quantidadeCedulas[j]} nota(s) de R$${tiposCedulas[j]},00`;
+                if (cedulas[j].quantidade === 1) {
+                    retorno[j] = ` ${cedulas[j].quantidade} nota de R$${cedulas[j].cedula},00`;
+                } else if (cedulas[j].quantidade > 1) {
+                    retorno[j] = ` ${cedulas[j].quantidade} notas de R$${cedulas[j].cedula},00`;
                 }
+            }
+        }
+
+        if (retorno.length === 1) {
+            return frase + retorno[0];
+        } else if (retorno.length > 1){
+            for(var e = 0; e < retorno.length; e++) {
+                if (e === retorno.length -1) {
+                    frase = frase + ' e' + retorno[e];
+                } else if (retorno[e] !== undefined) {
+                    frase += retorno[e];
+                } 
             }
         } else {
             return 'Abaixo do valor permitido.';
-        }
-        
-        return retorno;
+        }        
+        return frase;
     }
 }
--- saque.test.js
+++ saque.test.js
@@ -12,24 +12,20 @@ test('two plus two is four', () => {
 
 test('Comparar quantidade de cédulas no array de retorno para 20', () => {
   var resultado = saque.sacar(20);
-  expect(resultado).toContain(1);
-});
-
-test('Comparar quantidade de cédulas no array de retorno para 50', () => {
-  var resultado = saque.sacar(50);
-  expect(resultado).toContain(1);
+  expect(resultado[2].quantidade).toEqual(1);
 });
 
 test('Comparar quantidade de cédulas no array de retorno para 300', () => {
   var resultado = saque.sacar(300);
-  expect(resultado).toContain(3);
+  expect(resultado[0].quantidade).toEqual(3);
 });
 
 test('Compara mais de uma quantidade de cédulas no array de quantidade', () => {
   var resultado = saque.sacar(340);
   
-  expect(resultado).toContain(2);
-  expect(resultado).toContain(3);
+  expect(resultado[0].quantidade).toEqual(3);
+  expect(resultado[1].quantidade).toEqual(0);
+  expect(resultado[2].quantidade).toEqual(2);
 });
 
 test('sacar valor abaixo de 10', () => {
@@ -39,6 +35,11 @@ test('sacar valor abaixo de 10', () => {
 });
 
 test('sacar valor com uma nota de 30', () => {
-  var resultado = saque.formataTexto([0,0,1,1]);
-  expect(resultado).toMatch('Entregar 1 nota(s) de R$20,00 1 nota(s) de R$10,00');
+  var resultado = saque.formataTexto([
+    {cedula: 100, quantidade: 0},
+    {cedula: 50, quantidade: 0},
+    {cedula: 20, quantidade: 1},
+    {cedula: 10, quantidade: 1}
+  ]);
+  expect(resultado).toMatch('Entregar 1 nota de R$20,00 e 1 nota de R$10,00');
 });
